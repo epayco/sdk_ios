bd3937f649257cc3b8afc2752149c142fbc68820
JuanEstebanGarciaV <juan.garcia@epayco.com>
2025-06-09 13:41:54 -0500

    Se realiza sintaxis en endpoints

--- Sources/epayco-ios/Constants.swift
+++ Sources/epayco-ios/Constants.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 public struct K {
-    public static let urlBase = "https://api.secure.payco.co"
-    public static let baseUrlSecure = "https://secure.payco.co"
+    public static let urlBase = "https://api.secure.payco.co";
+    public static let baseUrlSecure = "https://secure.payco.co";
 }
