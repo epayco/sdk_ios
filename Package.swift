5d27317a08bdc23e162d16d1da1fa609f7f2650e
JuanEstebanGarciaV <juan.garcia@epayco.com>
2025-10-03 14:32:14 -0500

    feat: actualiza packages.swift

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "epayco-ios",
+    name: "epaycoSdkIos",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "epayco-ios",
-            targets: ["epayco-ios"]),
+            name: "epaycoSdkIos",
+            targets: ["epaycoSdkIos"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,11 +19,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "epayco-ios",
+            name: "epaycoSdkIos",
             dependencies: []
             ),
         .testTarget(
-            name: "epayco-iosTests",
-            dependencies: ["epayco-ios"]),
+            name: "epaycoSdkIosTests",
+            dependencies: ["epaycoSdkIos"]),
     ]
 )
