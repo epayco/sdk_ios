c8e1bc5a1bcb8d2192aa30446dbd1973c32c6dde
JuanEstebanGarciaV <juan.garcia@epayco.com>
2025-10-30 13:38:11 -0500

    feat: agregar nombres de campos correctos

--- Sources/epayco-ios/Models/Bank/NewBankTransactionCallModel.swift
+++ Sources/epayco-ios/Models/Bank/NewBankTransactionCallModel.swift
@@ -32,7 +32,41 @@ public struct NewBankTransactionCallModel: Encodable {
     public let lenguaje: String?
     public let i: String?
     public let extras_epayco: ExtrasModel?
-    
+
+    enum CodingKeys: String, CodingKey {
+        case banco = "bankCode"
+        case factura = "idfactura"
+        case descripcion = "description"
+        case valor = "amount"
+        case iva = "tax"
+        case baseiva = "taxBase"
+        case moneda = "currency"
+        case tipo_persona = "typePerson"
+        case tipo_doc = "documentType"
+        case documento = "document"
+        case nombres = "names"
+        case apellidos = "lastNames"
+        case email = "email"
+        case pais = "country"
+        case celular = "cellPhone"
+        case url_respuesta = "urlResponse"
+        case url_confirmacion = "urlConfirmation"
+        case metodoconfirmacion = "confirmationMethod"
+        case extra1 = "extra1"
+        case extra2 = "extra2"
+        case extra3 = "extra3"
+        case extra4 = "extra4"
+        case extra5 = "extra5"
+        case extra6 = "extra6"
+        case extra7 = "extra7"
+        case public_key = "publicKey"
+        case enpruebas = "test"
+        case ip = "ip"
+        case lenguaje = "language"
+        case i = "i"
+        case extras_epayco = "extras_epayco"
+    }
+
     public init(banco: String?, factura: String?, descripcion: String?, valor: String?, iva: String?, baseiva: String?, moneda: String?, tipo_persona: String?, tipo_doc: String?, documento: String?, nombres: String?, apellidos: String?, email: String?, pais: String?, celular: String?, url_respuesta: String?, url_confirmacion: String?, metodoconfirmacion: String?, extra1: String?, extra2: String?, extra3: String?, extra4: String?, extra5: String?, extra6: String?, extra7: String?, public_key: String?, enpruebas: String?, ip: String?, lenguaje: String?, i: String?, extras_epayco: ExtrasModel? = nil) {
         self.banco = banco
         self.factura = factura
